ff2474996a5e170d9d20d3d264bc2215c2a93f0a
dpisati <43629737+dpisati@users.noreply.github.com>
2020-12-19 09:00:58 +1300

    fixed header and tag switch added

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ import Card from "../components/Card";
 import Modal from "../components/Modal";
 import ConfirmationModal from "../components/ConfirmationModal";
 
+import Switch from "react-switch";
+
 export default function Home() {
   const [posts, setPosts] = useState([]);
   const [searchInput, setSearchInput] = useState("");
@@ -214,13 +216,25 @@ export default function Home() {
                 ></input>
               </div>
               <div className={styles.searchInputTagContainer}>
-                <input
-                  type="checkbox"
-                  value={searchForTags}
+
+                <Switch
+                  checked={searchForTags}
                   onChange={() => setSearchForTags(!searchForTags)}
+                  onColor="#86d3ff"
+                  onHandleColor="#2693e6"
+                  handleDiameter={30}
+                  uncheckedIcon={false}
+                  checkedIcon={false}
+                  boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                  activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                  height={20}
+                  width={48}
+                  className={styles.reactSwitch}
+                  id="material-switch"
                 />
+                 
                 <label className={styles.searchLabel}>
-                  search in tags only
+                  tags only
                 </label>
               </div>
             </div>
